08900430477e5bea1ed0b22311a46b845dd85331
anusreesaji <anusreesaji009@gmail.com>
2020-09-18 18:05:31 +0530

    func def

---
+++ main.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include"add.h"
+
+void main()
+{
+    int a,b,c;
+    c= add(a,b);
+    printf("%d",c);
+}
---
+++ src/add.c
@@ -0,0 +1,8 @@
+  
+#include "adder.h"
+#include <stdio.h>
+
+int adder(int a, int b)
+{
+    return (a+b);
+}
